034dd05ade12690b6a655d4e59cc7110d2d0a90a
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 16:26:08 +0200

    Rokas

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { HomeIcon } from '@heroicons/react/outline';
+
 
 const Sidebar = () => {
     return (
         <div>
-            
+            <div>
+                <button>
+                    <HomeIcon className="h-5 w-5" />
+                </button>
+            </div>
         </div>
     );
 };
