5fbf43cc9aff708b2c2847fe8b72ee4e90dfe3c8
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 16:52:05 +0200

    Roaks

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="">
+            <h1>This is login page</h1>
+        </div>
+    );
+};
+
+export default Login;
