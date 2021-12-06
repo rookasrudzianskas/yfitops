56299df8b36a3faa36b999d54a5a6fe26874cf2b
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 16:24:49 +0200

    Rokas

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify ✳️</title>
+      </Head>
+
+      <main>
+        <Sidebar />
+      </main>
+
+      <div>
+
+      </div>
+
+    </div>
+  )
+}
