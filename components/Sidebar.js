e85bb3a1b2db2753bb164baad641c42c6bc49ded
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 16:33:09 +0200

    Rokas

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { HomeIcon } from '@heroicons/react/outline';
+import {HomeIcon, LibraryIcon, SearchIcon} from '@heroicons/react/outline';
 
 
 const Sidebar = () => {
     return (
-        <div>
-            <div>
-                <button>
+        <div className="text-gray-500">
+            <div className="">
+                <button className="flex items-center space-x-2 hover:text-white">
                     <HomeIcon className="h-5 w-5" />
+                    <p>Home</p>
+                </button>
+
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <SearchIcon className="h-5 w-5" />
+                    <p>Search</p>
+                </button>
+
+                <button className="flex items-center space-x-2 hover:text-white">
+                    <LibraryIcon className="h-5 w-5" />
+                    <p>Your Library</p>
                 </button>
             </div>
         </div>
