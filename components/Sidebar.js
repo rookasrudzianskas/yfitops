33ea6edd0fed4fd53a868cd336530136b0b49b71
Rokas <rookas.rudzenskas@gmail.com>
2021-12-06 16:45:30 +0200

    ROkas

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {HomeIcon, LibraryIcon, SearchIcon} from '@heroicons/react/outline';
+import {HeartIcon, HomeIcon, LibraryIcon, RssIcon, SearchIcon} from '@heroicons/react/outline';
+import {PlusCircleIcon} from "@heroicons/react/outline";
 
 
 const Sidebar = () => {
@@ -24,20 +25,19 @@ const Sidebar = () => {
                 <hr className="border-t-[0.1px] border-gray-900" />
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <HomeIcon className="h-5 w-5" />
+                    <PlusCircleIcon className="h-5 w-5" />
                     <p>Create Playlist</p>
                 </button>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <SearchIcon className="h-5 w-5" />
-                    <p>Search</p>
+                    <HeartIcon className="h-5 w-5" />
+                    <p>Liked Songs</p>
                 </button>
 
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <LibraryIcon className="h-5 w-5" />
-                    <p>Your Library</p>
+                    <RssIcon className="h-5 w-5" />
+                    <p>Your episodes</p>
                 </button>
-
                 <hr className="border-t-[0.1px] border-gray-900" />
             </div>
         </div>
