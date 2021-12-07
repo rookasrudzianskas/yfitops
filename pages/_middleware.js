2839d9a22e84e7bdc7e33003b6a64eaaa85e8a3b
Rokas <rookas.rudzenskas@gmail.com>
2021-12-07 17:37:04 +0200

    Rokas

---
+++ pages/_middleware.js
@@ -0,0 +1,21 @@
+import {NextRequest, NextResponse} from 'next/server'
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req) {
+    // token will exists if the user is logged in
+    const token = await getToken({req, secret: process.env.JWT_SECRET});
+    const {pathname} = req.nextUrl;
+
+    // if the user is logged in, add the token to the request
+    // if the token is, I let you to go
+    if(pathname.includes('/api/auth') || token) {
+        return NextResponse.next();
+    }
+
+    // redirect them to login if they dont have a token AND requesting route is a protected route
+
+    if(token && pathname !== '/login') {
+        return NextResponse.redirect('/login');
+    }
+
+}
