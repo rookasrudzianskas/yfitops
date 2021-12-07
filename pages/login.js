754862825b45e9487efa27b4804e405e1fff9c91
Rokas <rookas.rudzenskas@gmail.com>
2021-12-07 16:42:03 +0200

    Rokas

--- pages/login.js
+++ pages/login.js
@@ -3,11 +3,11 @@ import {getProviders} from "next-auth/react";
 
 const Login = ({providers}) => {
     return (
-        <div className="">
+        <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <img src="https://links.papareact.com/9xl" className="w-52 mb-5" alt=""/>
             {Object.values(providers).map((provider, index) => (
-                <div>
-                    <button>Login with {provider.name}</button>
+                <div key={provider.name} >
+                    <button className="bg-[#18D860] text-white p-5 rounded-lg">Login with {provider.name}</button>
                 </div>
             ))}
         </div>
