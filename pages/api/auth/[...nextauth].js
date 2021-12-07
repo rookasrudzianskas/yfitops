011e0177492d863008abe5bc5b18911050d0a9ae
Rokas <rookas.rudzenskas@gmail.com>
2021-12-07 13:44:42 +0200

    Rokas

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ export default NextAuth({
         SpotifyProvider({
             clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-            authorizationEndpoint: "https://accounts.spotify.com/authorize",
+            authorization: LOGIN_URL,
         }),
         // ...add more providers here
     ],
