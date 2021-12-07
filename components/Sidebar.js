354bc482509cb097c497f0a4852ffb75ccb9d027
Rokas <rookas.rudzenskas@gmail.com>
2021-12-07 17:14:39 +0200

    Rokas

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,6 +6,7 @@ import {signOut, useSession} from "next-auth/react";
 
 const Sidebar = () => {
     const {data: session} = useSession();
+    // console.log(session)
 
     return (
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
