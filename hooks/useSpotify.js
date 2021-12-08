29d9f3533ace6e0f6e3df3ad1004bf6c09561ebf
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 10:14:50 +0200

    Rokas

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,6 +1,13 @@
 import React, {useEffect} from 'react';
 import {signIn, useSession} from "next-auth/react";
 import spotifyApi from "../lib/spotify";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+});
 
 const useSpotify = () => {
     const {data: session, status} = useSession();
