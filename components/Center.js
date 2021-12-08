230699ccfa8986bb85319023e33c0ae6d80bd889
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 11:03:50 +0200

    Rokas

--- components/Center.js
+++ components/Center.js
@@ -5,6 +5,7 @@ import {shuffle} from "lodash";
 import {useRecoilState} from "recoil";
 import {playlistIdState, playlistState} from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
     'from-indigo-500',
@@ -55,6 +56,11 @@ const Center = () => {
                     <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
                 </div>
             </section>
+
+            <div>
+                <Songs />
+            </div>
+
         </div>
     );
 };
---
+++ components/Songs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Songs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Songs;
