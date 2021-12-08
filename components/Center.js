6ece9edde5e49cf23056649e0ac2d89536ab8cb3
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 09:59:29 +0200

    Rokas

--- components/Center.js
+++ components/Center.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {useSession} from "next-auth/react";
+import {ChevronDownIcon} from "@heroicons/react/outline";
 
 const Center = () => {
+
+    const {data: session} = useSession();
+
     return (
-        <div className="flex flex-grow text-white">
-            <h1 className="text-white">I am center</h1>
+        <div className="flex-grow">
+            <header className="absolute top-5 right-8">
+                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+                    <img className="rounded-full w-10 h-10" src={session?.user?.image || 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg'} alt=""/>
+                    <h2>{session?.user?.name}</h2>
+                    <ChevronDownIcon className="h-5 w-5" />
+                </div>
+            </header>
+
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}>
+                {/*<img src={} alt=""/>*/}
+            </section>
         </div>
     );
 };
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = () => {
     // console.log(session)
 
     return (
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen">
+        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
             <div className="space-y-4">
                 <button onClick={() => signOut()} className="flex items-center space-x-2 hover:text-white">
                     <p>Log Out</p>
