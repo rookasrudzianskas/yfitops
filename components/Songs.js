431d67f5b8c3c47a2676c6301368c9f922f57e08
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 11:37:47 +0200

    Rokas

--- components/Songs.js
+++ components/Songs.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {playlistState} from "../atoms/playlistAtom";
+import {useRecoilValue} from "recoil";
 
 const Songs = () => {
+    const playlist = useRecoilValue(playlistState);
+
     return (
         <div className="">
-
+            {playlist?.tracks.items.map((track, index) => (
+                <div className="" key={index}>
+                    {track.track.name}
+                </div>
+            ))}
         </div>
     );
 };
