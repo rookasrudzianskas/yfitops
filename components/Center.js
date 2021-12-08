df82b6ef0bcda152fc3874a32c7c9052534bd01a
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 10:04:27 +0200

    ROkas

--- components/Center.js
+++ components/Center.js
@@ -26,7 +26,7 @@ const Center = () => {
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
                     <img className="rounded-full w-10 h-10" src={session?.user?.image || 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg'} alt=""/>
-                    <h2>{session?.user?.name}</h2>
+                    <h2 className="text-white">{session?.user?.name}</h2>
                     <ChevronDownIcon className="h-5 w-5" />
                 </div>
             </header>
