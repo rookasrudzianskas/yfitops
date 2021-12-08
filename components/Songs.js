510a7616cdd44c70498d384afb500d5ad0514dad
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 11:53:49 +0200

    Rokas

--- components/Songs.js
+++ components/Songs.js
@@ -7,7 +7,7 @@ const Songs = () => {
     const playlist = useRecoilValue(playlistState);
 
     return (
-        <div className="text-white">
+        <div className="text-white px-8 flex-col space-y-1 pb-28">
             {playlist?.tracks.items.map((track, index) => (
                 <Song key={index} track={track} order={index} />
             ))}
