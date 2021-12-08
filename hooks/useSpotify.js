73de2a4eb0cfabce943e44eb8525d5fa7178a6bc
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 10:11:04 +0200

    Rokas

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {useSession} from "next-auth/react";
+import React, {useEffect} from 'react';
+import {signIn, useSession} from "next-auth/react";
 
 const useSpotify = () => {
     const {data: session, status} = useSession();
 
-    return (
-        <div>
-
-        </div>
-    );
+    useEffect(async () => {
+        if (session) {
+            // if refreshed access token fails, so direct the user to manual login again
+            if (session.error === 'RefreshAccessTokenError') {
+                await signIn();
+            }
+        }
+    }, [session]);
+    return null;
 };
 
 export default useSpotify;
