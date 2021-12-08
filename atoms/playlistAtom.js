e08999b070a0b687256847cec5a306f6a6a460ee
Rokas <rookas.rudzenskas@gmail.com>
2021-12-08 10:42:46 +0200

    Rokas

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,5 +1,10 @@
 import {atom} from 'recoil';
 
+export const playlistAtom = atom({
+    key: 'playlistAtomState',
+    default: null,
+});
+
 export const playlistIdState = atom({
     key: 'playlistIdState',
     default: '37i9dQZF1EUMDoJuT8yJsl',
